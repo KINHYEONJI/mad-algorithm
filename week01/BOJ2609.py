7a71a2bf001e1bd1cc5611adb0264c2929414741
seonghoho <chltjdgh3@naver.com>
2023-08-01 01:25:48 +0900

    solved 2609

---
+++ week01/BOJ2609.py
@@ -0,0 +1,10 @@
+a, b = map(int, input().split())
+
+arr=[]
+for i in range(1,min(a,b)+1):
+    if a%i == 0 and b%i ==0:
+        arr.append(i)
+max_ = max(arr)
+m_max = (a//max_)*(b//max_)*max_
+
+print(f'{max_}\n{m_max}')
