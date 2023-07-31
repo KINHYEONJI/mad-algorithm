860aa77a0aa80fd2d2897558aaaf388b4340ce7e
seonghoho <chltjdgh3@naver.com>
2023-08-01 00:48:16 +0900

    solved 11050

---
+++ week01/BOJ11050.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+#5C2 이면 5!을 2!(5-2)! 으로 나눈 결괏값
+mo = 1
+ja = 1
+# nCk는 n-k+1 ~ n까지 곱한 값이 분자, 
+# 1~k까지 곱한 값이 분모
+for i in range(n-k+1,n+1):
+    ja *= i
+for j in range(1,k+1):
+    mo *= j
+
+print(ja//mo)
