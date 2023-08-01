164605ec145eb015eca3265f3a637e6f06554a32
김영일 <sugo2753@gamil.com>
2023-08-01 19:50:08 +0900

    solved1110

---
+++ week01/KIMYOUNGIL/BOJ1110.py
@@ -0,0 +1,14 @@
+n=int(input())
+
+if n < 10:
+    n *= 10
+    
+num1 = n//10+n%10
+num2 = (n%10)*10+(num1%10)
+cnt = 1
+while num2 != n:
+    nump = num2//10+num2%10
+    num2 = (num2%10)*10+(nump%10)
+    cnt += 1
+    
+print(cnt)
