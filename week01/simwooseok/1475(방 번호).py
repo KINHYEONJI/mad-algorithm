1ab58023a1c751445a4d80ffe5d7395851190ef5
심우석 <dntjr5741@naver.com>
2023-08-02 23:11:59 +0900

    solved 1475.

--- week01/simwooseok/1475(방 번호).py
+++ week01/simwooseok/1475(방 번호).py
@@ -1,14 +1,16 @@
 num = list(map(int, input()))
 bucket = [0] * 10
 
+Max_buck = bucket[0]
+Max_69 = 0
 for i in range(len(num)):
-    if num[i] == 6 or num[i] == 9:
-        if bucket[6] == bucket[9]:
-            bucket[6] += 1
-        
-        else: bucket[9] += 1
-
-    else:
-        bucket[num[i]] += 1
-
-print(max(bucket))
+    bucket[num[i]] += 1
+for i in range(len(bucket)):
+    if i == 6 or i == 9:
+        Max_69 = round(bucket[6] + bucket[9] + 0.6) // 2
+    else : 
+        if bucket[i] > Max_buck:    
+            Max_buck = bucket[i]
+if Max_69 >= Max_buck:
+    print(Max_69)
+else: print(Max_buck)
