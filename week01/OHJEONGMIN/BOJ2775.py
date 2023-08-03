a4b4742e5ff5b3dcf87425a5a638f36d0b8cfb1c
OHJEONGMIN <dhwjdals1324@gmail.com>
2023-08-03 23:12:43 +0900

    solved

---
+++ week01/OHJEONGMIN/BOJ2775.py
@@ -0,0 +1,20 @@
+tc = int(input())
+for _ in range(tc):
+    floor_lst = []
+    k = int(input())
+    n = int(input())
+    row=[]
+    for i in range(n): #0층 배열 구해주기
+        row.append(i+1)
+    floor_lst.append(row)
+    SUM=0
+
+    for floor in range(0,k):    #1층부터 나머지 층 배열 구해주기
+        Sum_lst=[]
+        Sum=0
+        for room in range(n):
+            Sum+=floor_lst[floor][room]
+            Sum_lst.append(Sum)
+
+        floor_lst.append(Sum_lst)
+    print(floor_lst[k][n-1])
