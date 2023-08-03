b96bd37ef197b2c0f59547606300af0c68bd3ad5
seonghoho <chltjdgh3@naver.com>
2023-08-04 00:56:46 +0900

    주석 추가

--- week01/seonghoho/BOJ2167.py
+++ week01/seonghoho/BOJ2167.py
@@ -1,12 +1,12 @@
 N, M = map(int, input().split()) #N*M 배열 입력
 
 numbers = [list(map(int, input().split())) for _ in range(N)]
-
+# 테스트 케이스 갯수 입력
 K = int(input())
 
 for i in range(K):
     sum_ = 0
-    test_case = list(map(int, input().split()))
+    test_case = list(map(int, input().split())) # x1,y1,x2,y2 순서로 입력
     for y1 in range(test_case[1]-1, test_case[3]):
         for x1 in range(test_case[0]-1, test_case[2]):
             sum_ += numbers[x1][y1]
