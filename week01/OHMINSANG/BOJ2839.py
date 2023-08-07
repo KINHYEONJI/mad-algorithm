f038c184aeb48ba34d4a60ca32e79eca2be5fd84
minsangOh <flos9537@gmail.com>
2023-08-07 20:32:28 +0900

    solved 2839

--- week01/OHMINSANG/BOJ2839.py
+++ week01/OHMINSANG/BOJ2839.py
@@ -1,12 +1,15 @@
-n_str = input()
-n = int(n_str)
-
-if n % 15 == 0:
-    num = n // 5
-elif n< 15:
-    num = (((n // 5) * 5) // 5) + ((n - ((n // 15) * 15)) // 3)
-elif (n - ((n // 15) * 15)) % 3 != 0:
-    num = -1
-else:
-    num = (((n // 15) * 15) // 5) + ((n - ((n // 15) * 15)) // 3)
-print(num)
+n = int(input())
+cnt = 0
+# 무한 루프 형성
+while 1:
+    if n % 5 == 0:
+        cnt += n // 5
+        break
+    # 입력값이 5로 안나누어 떨어지면 -3 하고 카운트 +1
+    n -= 3
+    cnt += 1
+    # n이 0보다 작아질경우 -1 고정
+    if n < 0:
+        cnt = -1
+        break
+print(cnt)
