1316c6ea11ee86648a787dd8ac02a8283262f23b
김영일 <sugo2753@gamil.com>
2023-08-11 19:36:37 +0900

    solved 15650

---
+++ week01/KIMYOUNGIL/BOJ15650.py
@@ -0,0 +1,12 @@
+n,m = map(int, input().split())
+arr = list(range(1,n+1))
+path = [0]*m
+def dfs(x,y):
+    if x == m:
+        print(*path,sep=" ")
+        return
+    for i in range(y,n):
+        path[x] = arr[i]
+        dfs(x+1,i+1)
+
+dfs(0,0)
