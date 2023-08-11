ebf7c451e96e55badd11ef994d0c2aa2b5ce1eb4
김영일 <sugo2753@gamil.com>
2023-08-11 19:50:43 +0900

    solved 15654

--- week01/KIMYOUNGIL/BOJ15654.py
+++ week01/KIMYOUNGIL/BOJ15654.py
@@ -1,15 +1,12 @@
 n,m = map(int, input().split())
 arr = list(range(1,n+1))
 path = [0]*m
-# used = [0]*(n+1)
 def dfs(x,y):
     if x == m:
         print(*path,sep=" ")
         return
     for i in range(y,n):
-        # if used[i] == 0:
         path[x] = arr[i]
         dfs(x+1,i)
-        # used[i] = 1
 
 dfs(0,0)
