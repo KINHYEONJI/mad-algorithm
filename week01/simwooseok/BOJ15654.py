e322866ec9d25f89a7fd24c81acca0f45c8ec512
심우석 <dntjr5741@naver.com>
2023-08-14 00:13:36 +0900

    solved 15654

---
+++ week01/simwooseok/BOJ15654.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr2 = sorted(arr)
+path = ['']*M
+used = [0] *N
+def abc(level):
+    if level == M:
+        for i in range(len(path)):
+            print(path[i], end = ' ')
+        print()
+        return
+    
+    for i in range(len(arr)):
+        if used[i] == 1: continue
+        used[i] = 1
+        path[level] = arr2[i]
+        abc(level + 1)
+        used[i] = 0
+abc(0)
