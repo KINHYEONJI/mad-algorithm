49873bc80a9130e3e48d4e2829db06f3d2353308
minsangOh <flos9537@gmail.com>
2023-08-14 17:55:19 +0900

    solving...

--- week02/OHMINSANG/BOJ11866.py
+++ week02/OHMINSANG/BOJ11866.py
@@ -2,20 +2,16 @@
 요세푸스 문제()
 0부터 len(n)까지, for 문 돌려서 k-1스탭씩 가서 버리고, i > len(k)일 때는 나눠서 나머지의 인덱스 번호에 해당 값 제거
 """
+from collections import deque
 n, k = map(int, input().split())
 lst = []
 for j in range(1, n + 1):
     lst.append(j)
-k = k - 1
-lstt = []
+cnt = 0
+q = deque()
 
-while len(lst) > 0:
-    k = k % len(lst)
-    x = lst.pop(k)
-    lstt.append(x)
-    k += 2
-    if k >= len(lst):
-        k -= 1
-
-
-print(f'<{", ".join(map(str, lstt))}>')
+while len(lst) > cnt:
+    for _ in range(3):
+        q.append(lst)
+    now = q.pop()
+    print(now)
