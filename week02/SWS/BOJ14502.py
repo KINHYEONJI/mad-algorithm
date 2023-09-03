d929e41b1bef23d0a7d080987512f743acb898db
심우석 <dntjr5741@naver.com>
2023-09-04 01:01:26 +0900

    solved 14502

--- week02/SWS/BOJ14502.py
+++ week02/SWS/BOJ14502.py
@@ -1,24 +1,47 @@
+import copy
 from collections import deque
+
+
+directy = [0,0,1,-1]
+directx = [1,-1,0,0]
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-deq = deque()
 
-directy = [0,0,1,-1, 0]
-directx = [1,-1,0,0, 0]
+q = deque()
+ans = 0
 
-def virus(i, j):
-    for k in range(5):
-        dy = i + directy[k]
-        dx = j + directx[k]
-        if dx < 0 or dy < 0 or dx > N-1 or dy > M-1:continue
-        if arr[dy][dx] == 1: continue
-        arr[dy][dx] = 2
+def abc():
+    global ans
+    w = copy.deepcopy(arr)
+    for i in range(N):
+        for j in range(M):
+            if w[i][j]==2:
+                q.append([i,j])
 
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] == 2:
-            virus(i, j)
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            dy = y + directy[i]
+            dx = x + directx[i]
+            if 0 <= dx < N and 0 <= dy < M:
+                if w[dx][dy]==0:
+                    w[dx][dy] = 2
+                    q.append([dx,dy])
+    cnt = 0
+    for i in w:
+        cnt+=i.count(0)
+    ans = max(ans,cnt)
 
-print()
-for i in arr:
-    print(*i)
+
+def wall(x):
+    if x == 3:
+        abc()
+        return
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 0:
+                arr[i][j] = 1
+                wall(x + 1)
+                arr[i][j] = 0
+wall(0)
+print(ans)
