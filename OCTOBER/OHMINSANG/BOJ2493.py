6603612f44e7bfdc90aa679ad15b5acfcb826f77
minsangOh <flos9537@gmail.com>
2023-09-05 00:30:36 +0900

    solved 2493, code fix

--- OCTOBER/OHMINSANG/BOJ2493.py
+++ OCTOBER/OHMINSANG/BOJ2493.py
@@ -3,18 +3,18 @@ sys.stdin = open('input.txt', 'r')
 
 n = int(input())
 lst = list(map(int, input().split()))
-q = []
+stack = []
 used = [0]* n
 for i in range(n):
-    while q:
+    while stack:
         """
         q의 최상단에 입력된 값이 lst의 인덱스번호가 된다.
         q의 해당 인덱스 값과 lst[i]를 비교해서  lst가 더 크면 pop
         """
-        if lst[q[-1][1]] < lst[i]:
-            q.pop()
+        if lst[stack[-1][1]] < lst[i]:
+            stack.pop()
         else:
-            used[i] = q[-1][1]+1    # 출력 되야 하는 값 입력
+            used[i] = stack[-1][1] + 1    # 출력 되야 하는 값 입력
             break
-    q.append([lst[i], i])   # 큐 쌓기
+    stack.append([lst[i], i])   # 스택 쌓기
 print(*used)
