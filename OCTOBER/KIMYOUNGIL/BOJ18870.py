8aef75669fd0010285d28db20c67fdc0dd989da3
김영일 <sugo2753@gamil.com>
2023-09-07 00:06:32 +0900

    solved 18870

--- OCTOBER/KIMYOUNGIL/BOJ18870.py
+++ OCTOBER/KIMYOUNGIL/BOJ18870.py
@@ -8,8 +8,6 @@ lst.sort()
 
 ans = [0]*n
 for i in range(1,n):
-    if lst[i-1][0] < lst[i][0]:
-        ans[lst[i][1]] = ans[lst[i-1][1]] + 1
-    else:
-        ans[lst[i][1]] = ans[lst[i-1][1]]
+    if lst[i-1][0] < lst[i][0]: ans[lst[i][1]] = ans[lst[i-1][1]] + 1
+    else: ans[lst[i][1]] = ans[lst[i-1][1]]
 print(*ans)
