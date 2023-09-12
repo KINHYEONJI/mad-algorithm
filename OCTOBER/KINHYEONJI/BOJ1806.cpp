7f0dcde6abc1f26851b2089945f4eea7f5535386
KINHYEONJI <hyeonji0718@gmail.com>
2023-09-12 16:10:12 +0900

    solved 1806

---
+++ OCTOBER/KINHYEONJI/BOJ1806.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, s, tot;
+int a[100005];
+int mn = 0x7fffffff;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> s;
+    for(int i = 0; i < n; i++) cin >> a[i];
+    tot = a[0];
+    int en = 0;
+    for(int st = 0; st < n; st++){
+        while(en < n && tot < s){
+            en++;
+            if(en != n) tot += a[en];
+        }
+        if(en == n) break;
+        mn = min(mn, en - st + 1);
+        tot -= a[st];
+    }
+    if(mn == 0x7fffffff) mn = 0;
+    cout << mn;
+}
