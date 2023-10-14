e37b4130c6e61d873f8034f047f60292f2bf79c1
giseon <pakox123@gmail.com>
2023-10-14 23:52:58 +0900

    solved

--- OCTOBER/KIMYOUNGIL/BOJ6198.py
+++ OCTOBER/KIMYOUNGIL/BOJ6198.py
@@ -1,15 +1,15 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
-
+#스택 나보다 작은건물 다 빼면 나를 볼 수 있는 건물 세는 양식
 n = int(input())
-arr = deque(int(input()) for _ in range(n))
-lst = []
+ 
+arr = [int(input()) for i in range(n)]
+stk = []
 ans = 0
-while arr:
-    p = arr.popleft()
-    while lst and lst[-1] <= p:
-        lst.pop()
-    lst.append(p)
-    ans += len(lst) - 1
+ 
+for i in range(n):
+    while stk and stk[-1] <= arr[i]:
+        stk.pop()
+ 
+    stk.append(arr[i])
+    ans += len(stk) -1
+ 
 print(ans)
