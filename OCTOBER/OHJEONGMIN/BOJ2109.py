98bc2ed233f49c0f3ac20efe71f0a14461c63554
ohjeongmin <dhwjdals1324@gmail.com>
2023-10-18 00:11:08 +0900

    solved 2109

---
+++ OCTOBER/OHJEONGMIN/BOJ2109.py
@@ -0,0 +1,19 @@
+#max heap
+import sys
+import heapq
+input = sys.stdin.readline
+n = int(input())
+lst = []
+for i in range(n):
+    p,d = map(int,input().split())
+    lst.append([p,d])
+money = [0]*10001
+lst.sort(key=lambda x:(-x[0],x[1]))
+for i in lst:
+    pay,day = i
+    for k in range(day,0,-1):
+        if money[k]==0:
+            money[k]=pay
+            break
+
+print(sum(money))
