b387845b941160b5c5d24d6308f76e1f9a6aa2e4
김영일 <sugo2753@gamil.com>
2023-10-26 21:22:50 +0900

    sovled 2011

---
+++ OCTOBER/KIMYOUNGIL/BOJ2011.py
@@ -0,0 +1,12 @@
+n = input()
+m = len(n)
+if n[0] == '0':print(0)
+else:
+    dp = [0]*(m+1)
+    dp[0],dp[1] = 1,1
+    for i in range(2,m+1):
+        one = int(n[i-1])
+        two = int(n[i-2])*10 + one
+        if one > 0: dp[i] += dp[i-1]
+        if 10 <= two <= 26: dp[i] += dp[i-2]
+    print(dp[m]%1000000)
