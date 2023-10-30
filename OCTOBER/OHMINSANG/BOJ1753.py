5adb3ea3bbc11ba5b8a7731b81d660f246e2a4a8
minsangOh <flos9537@gmail.com>
2023-10-30 21:20:47 +0900

    solved 1753

---
+++ OCTOBER/OHMINSANG/BOJ1753.py
@@ -0,0 +1,39 @@
+"""
+23 / 10 / 30 알고 스터디
+최단경로
+"""
+import sys, heapq
+def input(): return sys.stdin.readline().rstrip()
+
+def dijkstra(start):
+    hq = []
+    heapq.heappush(hq, (0, start))
+    result[start] = 0
+
+    while hq:
+        cost_k, k = heapq.heappop(hq)
+        if cost_k > result[k]:
+            continue
+        for lst in arr[k]:
+            cost = cost_k + lst[0]
+            if cost < result[lst[1]]:
+                result[lst[1]] = cost
+                heapq.heappush(hq, (cost, lst[1]))
+
+
+point, line = map(int, input().split())
+start = int(input())
+arr = [[] for _ in range(point + 1)]
+for _ in range(line):
+    s, e, cost = map(int, input().split())
+    arr[s].append((cost, e))
+
+inf = int(12e12)
+result = [inf] * (point + 1)
+
+dijkstra(start)
+result.pop(0)
+for i in range(len(result)):
+    if result[i] == inf:
+        result[i] = 'INF'
+    print(result[i])
