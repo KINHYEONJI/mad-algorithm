30563f7cccccc2d9b4fc72eadf3da9202aee9d28
KINHYEONJI <hyeonji0718@gmail.com>
2023-11-01 16:26:09 +0900

    resolved 9205

--- NOVEMBER/KINHYEONJI/BOJ9205.cpp
+++ NOVEMBER/KINHYEONJI/BOJ9205.cpp
@@ -18,31 +18,31 @@ int main() {
         cin >> home_x >> home_y;
         vector<pair<int, int>> con_v;
         vector<int> visited(n, 0);
-        while(n--){
+        for (int i = 0; i < n; i++) {
             int con_x, con_y;
-            cin>>con_x>>con_y;
+            cin >> con_x >> con_y;
             con_v.push_back({con_x, con_y});
         }
         cin >> fest_x >> fest_y;
         queue<pair<int, int>> q;
         int is_happy = 0;
         q.push({home_x, home_y});
-        while(!q.empty()){
+        while (!q.empty()) {
             auto cur = q.front();
             q.pop();
-            for(int i = 0; i<n; i++){
-                if(abs(con_v[i].first - cur.first) + abs(con_v[i].second - cur.second) <= 1000){
-                    if(!visited[i]){
+            for (int i = 0; i < n; i++) {
+                if (abs(con_v[i].first - cur.first) + abs(con_v[i].second - cur.second) <= 1000) {
+                    if (!visited[i]) {
                         visited[i] = 1;
                         q.push(con_v[i]);
                     }
                 }
             }
-            if(abs(fest_x - cur.first) + abs(fest_y - cur.second) <= 1000){
+            if (abs(fest_x - cur.first) + abs(fest_y - cur.second) <= 1000) {
                 is_happy = 1;
                 break;
             }
         }
-        is_happy ? cout<<"happy\n" : cout<<"sad\n";
+        is_happy ? cout << "happy\n" : cout << "sad\n";
     }
 }
