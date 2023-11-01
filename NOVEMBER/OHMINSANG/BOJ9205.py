f59d5b7eb2d0f2dba0b204c0fe798373a11764b1
minsangOh <flos9537@gmail.com>
2023-11-01 21:03:22 +0900

    solved 9205

--- NOVEMBER/OHMINSANG/BOJ9205.py
+++ NOVEMBER/OHMINSANG/BOJ9205.py
@@ -2,32 +2,32 @@
 23 / 11 / 01 알고 스터디
 맥주 마시면서 걸어가기
 """
-from collections import deque
 import sys
 def input(): return sys.stdin.readline().rstrip()
 
 
 def recursive(x, y):
     if abs(x - rx) + abs(y - ry) <= 1000:
-        print('happy')
-        return
+        return 1
     else:
-        if arr:
-            xx, yy = arr.popleft()
+        for lst in enumerate(arr):
+            i, (xx, yy) = lst
+            if visited[i] == 1: continue
             if abs(x - xx) + abs(y - yy) <= 1000:
-                recursive(xx, yy)
-            # 편의점까지 못가면
-            else:
-                print('sad')
-        # 편의점 다 돌았는데도 못가면
-        else:
-            print('sad')
-        # 편의점을 팝 하지말고 편의점 포문 돌아서 되는 조건으로 하고 방문 편의점 방문 체크
+                visited[i] = 1
+
+                # 재귀 호출 값이 1일때만 리턴받기
+                if recursive(xx, yy): return 1
+    return 0
+
 
 TC = int(input())
 for _ in range(TC):
     n = int(input())
-    x, y = map(int, input().split())
-    arr = deque([list(map(int, input().split())) for _ in range(n)])
+    hx, hy = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(n)]
     rx, ry = map(int, input().split())
-    recursive(x, y)
+    visited = [0] * n
+    ans = recursive(hx, hy)
+    if ans == 1: print('happy')
+    else: print('sad')
