47952742ecb012cf863d0499fb5eeadefea0bf99
leeseunghyeon <tmdgus1761@gmail.com>
2023-11-02 16:38:30 +0900

    solved 11722

---
+++ NOVEMBER/LEESEUNGHYEON/BOJ11722.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+lst = [0] + list(map(int, input().split()))
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    mx = 0
+    for j in range(0, i):
+        if lst[i] < lst[j]:
+            mx = max(mx, dp[j])
+    dp[i] = mx + 1
+print(max(dp))
